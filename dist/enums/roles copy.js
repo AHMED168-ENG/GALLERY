a4d2e6bdae719a28be565628c953d344cfb66293
ahmed zaky <aalsaeed@nadeer.com.sa>
2024-01-26 20:59:58 +0200

    updates

--- dist/enums/roles copy.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Roles = void 0;
-var Roles;
-(function (Roles) {
-    Roles["User"] = "User";
-    Roles["Admin"] = "Admin";
-    Roles["SuperAdmin"] = "SuperAdmin";
-})(Roles = exports.Roles || (exports.Roles = {}));
